4848d695558ab4b55ec9d238a4053d67fa5f116a
OrionGiftShop <mariamisujashvili1@gmail.com>
2024-07-15 18:03:38 +0400

    ddd

--- src/Modules/ScssFooter.jsx
+++ src/Modules/ScssFooter.jsx
@@ -4,31 +4,35 @@ import js from "../FooterAssets/JS.png";
 import Ang from "../FooterAssets/Ang.png";
 import Reactimg from "../FooterAssets/React.png";
 import Vue from "../FooterAssets/Vue.png";
+import js1 from "../FooterAssets/Js1.svg";
+import Ang1 from "../FooterAssets/Ang.svg";
+import Reactimg1 from "../FooterAssets/React1.svg";
+import Vue1 from "../FooterAssets/Vue.svg";
 
 const ScssFooter = ({ isLight, setIsLight }) => {
   return (
     <footer className={isLight ? Style.myFooter : Style.myFooter1}>
       <div className={Style.Maindiv}>
         <section>
-          <img src={js} alt='' />
+          <img src={isLight ? js : js1} alt='' />
           <div className={Style.block}>
             <p>Java Script</p>
           </div>
         </section>
         <section>
-          <img src={Reactimg} alt='' />
+          <img src={isLight ? Reactimg : Reactimg1} alt='' />
           <div className={Style.block}>
             <p>React</p>
           </div>
         </section>
         <section>
-          <img src={Ang} alt='' />
+          <img src={isLight ? Ang : Ang1} alt='' />
           <div className={Style.block}>
             <p>Angular Js</p>
           </div>
         </section>
         <section>
-          <img src={Vue} alt='' />
+          <img src={isLight ? Vue : Vue1} alt='' />
           <div className={Style.block}>
             <p>Vue Js</p>
           </div>
